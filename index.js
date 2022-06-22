f8c4dabae18cbfbe4bc7fbd148034f763aab3709
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-22 08:45:31 +0300

    Arrays finnd method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W")
+
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
+
+
+
+
+//Find method
